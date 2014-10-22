ff03e3f86d237c6ba4098a78a600c85aa4e8c6b0
wenxian.tian <tianwx2003@126.com>
2014-10-22 11:09:20 +0800

    add some Action

--- core/src/main/scala/org/openurp/teach/domain/ProjectBean.scala
+++ core/src/main/scala/org/openurp/teach/domain/ProjectBean.scala
@@ -1,9 +1,11 @@
 package org.openurp.teach.domain
 
-import java.util.Date
+import scala.collection.mutable.Buffer
+
 import org.beangle.data.model.bean.IntIdBean
 import org.beangle.data.model.bean.LongIdBean
 import org.beangle.data.model.bean.NamedBean
+import org.beangle.data.model.bean.TemporalOnBean
 import org.openurp.base.Calendar
 import org.openurp.base.Campus
 import org.openurp.base.Department
@@ -11,14 +13,14 @@ import org.openurp.base.Room
 import org.openurp.base.School
 import org.openurp.base.TimeSetting
 import org.openurp.base.code.Education
-import org.openurp.teach.ProjectProperty
-import org.openurp.teach.code.StdType
 import org.openurp.base.domain.code.CodeMeta
-import org.openurp.teach.ProjectConfig
 import org.openurp.teach.Project
-import org.openurp.teach.code.StdLabel
-import org.openurp.teach.ProjectClassroom
 import org.openurp.teach.ProjectBasedObject
+import org.openurp.teach.ProjectClassroom
+import org.openurp.teach.ProjectConfig
+import org.openurp.teach.ProjectProperty
+import org.openurp.teach.code.StdLabel
+import org.openurp.teach.code.StdType
 /**
  * 项目
  * 
@@ -28,31 +30,31 @@ import org.openurp.teach.ProjectBasedObject
  * @has 1..* AssignedTo 1..* StdLabel
  * @depend - - - Calendar
  */
-class ProjectBean extends IntIdBean with NamedBean with Project  {
+class ProjectBean extends IntIdBean with NamedBean with TemporalOnBean with Project  {
 	/** 适用学校 */
   var school:School=_
   	/** 校区列表 */
- var campuses: List[Campus]=_
+ var campuses: Buffer[Campus]= new collection.mutable.ListBuffer[Campus]
  	/** 部门列表 */
- var departments:List[Department]=_
- 	/** 学历层次列表 */
- var educations:List[Education]=_
+ var departments:Buffer[Department]= new collection.mutable.ListBuffer[Department]
+   /** 学历层次列表 */
+ var educations:Buffer[Education]=new collection.mutable.ListBuffer[Education]
  	/** 学生分类列表 */
- var labels:List[StdLabel]=_
+ var labels:Buffer[StdLabel]=new collection.mutable.ListBuffer[StdLabel]
  /** 学生类别列表 */
- var types:List[StdType]=_
+ var types:Buffer[StdType]=new collection.mutable.ListBuffer[StdType]
  	/** 使用校历 */
  var calendar:Calendar=_
  	/** 小节设置 */
- var timeSettings:List[TimeSetting]=_
+ var timeSettings:Buffer[TimeSetting]=new collection.mutable.ListBuffer[TimeSetting]
  	/** 描述 */
  var description:String=_
  	/** 是否辅修 */
  var minor:Boolean=_
- 	/** 生效时间 */
- var effectiveAt:Date=_
-	/** 失效时间 */
- var invalidAt : Date=_
+// 	/** 生效时间 */
+// var effectiveAt:Date=_
+//	/** 失效时间 */
+// var invalidAt : Date=_
  
  
 
@@ -63,16 +65,13 @@ class ProjectBean extends IntIdBean with NamedBean with Project  {
  * @author chaostone
  *
  */
-class ProjectClassroomBean extends IntIdBean with ProjectBasedObject[Integer] with ProjectClassroom {
+class ProjectClassroomBean extends ProjectBasedObject[java.lang.Long] with ProjectClassroom {
   	/** 教室 */
   var room:Room=_
   	/** 固定使用部门 */
-  var departs:Set[Department]=_
+  var departs:collection.mutable.Set[Department]= new collection.mutable.HashSet[Department]
   /** 保留时间 */
 //  var reserved:List[TimeUnit]=_
-  
-   
-  
 } 
 
 /**
--- webapp/src/main/scala/org/openurp/teach/action/DefaultModule.scala
+++ webapp/src/main/scala/org/openurp/teach/action/DefaultModule.scala
@@ -1,20 +1,20 @@
 package org.openurp.teach.action
 
 import org.beangle.commons.inject.bind.AbstractBindModule
+import org.openurp.teach.action.code.DegreeAction
+import org.openurp.teach.action.code.DisciplineAction
+import org.openurp.teach.action.code.DisciplineCatalogAction
+import org.openurp.teach.action.code.StdLabelAction
+import org.openurp.teach.action.code.StdLabelTypeAction
 import org.openurp.teach.action.code.StdStatusAction
+import org.openurp.teach.action.code.StdTypeAction
+import org.openurp.teach.action.code.StudyTypeAction
 import org.openurp.teach.action.code.TeacherStateAction
+import org.openurp.teach.action.code.TeacherTitleAction
+import org.openurp.teach.action.code.TeacherTitleLevelAction
 import org.openurp.teach.action.code.TeacherTypeAction
 import org.openurp.teach.action.code.TeacherUnitTypeAction
 import org.openurp.teach.action.code.TutorTypeAction
-import org.openurp.teach.action.code.DisciplineCatalogAction
-import org.openurp.teach.action.code.DisciplineAction
-import org.openurp.teach.action.code.StudyTypeAction
-import org.openurp.teach.action.code.DegreeAction
-import org.openurp.teach.action.code.TeacherTitleAction
-import org.openurp.teach.action.code.TeacherTitleLevelAction
-import org.openurp.teach.action.code.StdTypeAction
-import org.openurp.teach.action.code.StdLabelTypeAction
-import org.openurp.teach.action.code.StdLabelAction
 
 class DefaultModule extends AbstractBindModule {
 
@@ -23,6 +23,8 @@ class DefaultModule extends AbstractBindModule {
     bind(classOf[DisciplineAction],classOf[DisciplineCatalogAction])
     bind(classOf[TeacherTitleAction],classOf[TeacherTitleLevelAction],classOf[StudyTypeAction],classOf[DegreeAction])
     bind(classOf[StdLabelAction],classOf[StdLabelTypeAction],classOf[StdTypeAction])
-    //bind()
+    bind(classOf[AdminclassAction],classOf[MajorAction],classOf[DirectionAction]
+    		,classOf[DirectionJournalAction],classOf[ProjectAction],classOf[MajorJournalAction])
+    bind(classOf[ProjectClassroomAction],classOf[ProjectConfigAction])
   }
 }
