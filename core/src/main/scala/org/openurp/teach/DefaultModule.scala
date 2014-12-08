9367407d39c6d0ab3f020061761e2592e9a20f87
xinzhou <xin.2165@gmail.com>
2014-12-08 09:53:41 +0800

    add gradeInputSwitch

--- core/src/main/scala/org/openurp/teach/DefaultModule.scala
+++ core/src/main/scala/org/openurp/teach/DefaultModule.scala
@@ -5,11 +5,13 @@ import org.openurp.teach.code.service.internal.BaseCodeServiceImpl
 import org.openurp.teach.grade.service.internal.DefaultGpaStatService
 import org.openurp.teach.grade.domain.impl.CourseGradeProviderImpl
 import org.openurp.teach.grade.domain.impl.DefaultGpaPolicy
+import org.openurp.teach.grade.service.internal.GradeInputSwitchServiceImpl
 
 class DefaultModule extends AbstractBindModule {
 
   protected override def binding(): Unit = {
     bind(classOf[BaseCodeServiceImpl])
     bind(classOf[DefaultGpaStatService],classOf[CourseGradeProviderImpl],classOf[DefaultGpaPolicy])
+    bind(classOf[GradeInputSwitchServiceImpl])
   }
 }
