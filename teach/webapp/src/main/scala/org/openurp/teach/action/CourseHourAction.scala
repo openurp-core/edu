ef2e90ac5ff4a33e94c50a86ddcb2ed6b281bab1
xinzhou <xin.2165@gmail.com>
2015-04-15 09:48:00 +0800

    delete actions in webapp

--- teach/webapp/src/main/scala/org/openurp/teach/action/CourseHourAction.scala
+++
@@ -1,7 +0,0 @@
-package org.openurp.teach.action
-
-import org.beangle.webmvc.entity.action.RestfulAction
-import org.openurp.teach.core.CourseHour
-
-class CourseHourAction extends RestfulAction[CourseHour]  
-
